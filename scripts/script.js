39e7af418e08e3ce4281ea948727b83c3b7e5382
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-01-10 21:33:39 -0500

    button is working for storing cities inside of my array. my api is working for pulling one city, need to get looping through more than one

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,46 @@
-// Shorthand for $( document ).ready()
+$(function() {
 
- console.log("ready!");
+ // Initial array of cities
+ var cities = [];
+ 
+ //working for pulling info for one citty, gotta figure out how to filter for mulitple. 
+ var city = "Las Vegas"; 
+ var queryURL = "http://api.openweathermap.org/data/2.5/find?q=" + city + "&units=imperial&appid=101220419d85ffb610459f1145df78ff";
 
-   // Initial array of cities
-   var cities = [];
+ $.ajax({
+ url: queryURL,
+ method: "GET"
+ }).then(function(response) {
+   console.log(response);
+  // $("form").append(JSON.stringify(response)); 
+ });
 
-   $("#searchBtn").on("click", function(event) {
+
+
+/*Looping through the array of cities 
+for (var i = 0; i < cities.length; i++) {
+    var a = $("<button>");
+    a.addClass("city");
+    a.attr("data-name", cities[i]);
+    a.text(cities[i]);
+    // Adding the button to the buttons-view div
+    $("pastCities").append(a);
+  } */
+
+$("#searchBtn").on("click", function(event) {
     event.preventDefault();
-
     // This line grabs the input from the textbox
     var city = $("#citySearch").val().trim();
-
     // The city from the textbox is then added to array
     cities.push(city);
     console.log(cities); 
    });
+ 
+   
 
+ // $(document).on("click", ".city", displayCityInfo);
+//end of document.ready*/
+}); 
 /*
 Search field/button
 id = citySearch 
