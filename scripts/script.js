7d2d9ca3dd004fbceea8a675f10b773783451961
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-01-10 19:48:56 -0500

    added css styling and psuedo code to js and css

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+/*
+Search field/button
+id = citySearch 
+id = searchBtn
+***need to make each city a button
+*Main content: 
+id=cityDisplay //City Heading with date image of that temp//all one, 
+this is same size different lines - 
+
+Temp F, Humidtity %, windspeed MPH, UV index text is highlighted
+id=temperature
+id=humidity
+id=windSpeed
+id=uvIndex;
+*5-Day Forecast */
+
