79c0e7c4efcf80ccbbdc58339a0832961ac93302
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-01-12 20:05:02 -0500

    five day forecast api linked and pushign day one info

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
 $(function() {
 
  // Initial array of cities
- var cities = [];
- 
- //working for pulling info for one citty, gotta figure out how to filter for mulitple. 
- 
+var cities = [];
+
  function displayCityInfo(){
  var city= $(this).attr("data-name"); 
  var queryURL = "http://api.openweathermap.org/data/2.5/find?q=" + city + "&units=imperial&appid=101220419d85ffb610459f1145df78ff";
+ var queryURL5Day = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=101220419d85ffb610459f1145df78ff"
 
  $.ajax({
  url: queryURL,
@@ -19,8 +18,42 @@ $(function() {
    $("#humidity").text("Humidity: " + response.list[0].main.humidity + " %"); 
    $("#windSpeed").text("Wind Speed: " + response.list[0].wind.speed + " MPH"); 
   // $("#uvIndex").text("UV Index: " + response.list)
+
+  //5 day
+ (response.list[0].name); 
 });
- }; 
+$.ajax({
+  url: queryURL5Day, 
+  method: "GET"
+}).then(function(response){
+//day 1 
+$("#day1").text(response.list[0].dt_txt); 
+//image
+$("#temp1").text("Temperature: "+ response.list[0].main.temp + " Fahrenheit");
+$("#humidity1").text("Humidity: " + response.list[0].main.humidity + " %"); 
+//day 2
+$("#day2").text(response.list[0].dt_txt); 
+//image
+$("#temp2").text("Temperature: "+ response.list[0].main.temp + " Fahrenheit");
+$("#humidity2").text("Humidity: " + response.list[0].main.humidity + " %"); 
+//day 3
+$("#day3").text(response.list[0].dt_txt); 
+//image
+$("#temp3").text("Temperature: "+ response.list[0].main.temp + " Fahrenheit");
+$("#humidity3").text("Humidity: " + response.list[0].main.humidity + " %"); 
+//day 4
+$("#day4").text(response.list[0].dt_txt); 
+//image
+$("#temp4").text("Temperature: "+ response.list[0].main.temp + " Fahrenheit");
+$("#humidity4").text("Humidity: " + response.list[0].main.humidity + " %"); 
+//day 5
+$("#day5").text(response.list[0].dt_txt); 
+//image
+$("#temp5").text("Temperature: "+ response.list[0].main.temp + " Fahrenheit");
+$("#humidity5").text("Humidity: " + response.list[0].main.humidity + " %"); 
+});
+ }
+
 
 //Looping through the array of cities 
 function renderButtons(){
@@ -36,17 +69,18 @@ for (var i = 0; i < cities.length; i++) {
 
 $("#searchBtn").on("click", function(event) {
     event.preventDefault();
+   //why isnt this stopping it
+    event.stopPropagation();
     // This line grabs the input from the textbox
     var city = $("#citySearch").val().trim();
     // The city from the textbox is then added to array
     cities.push(city);
-    console.log(cities); 
+    localStorage.setItem("cities", cities); 
     renderButtons(); 
    });
  
    
-
- $(document).on("click", ".city", displayCityInfo);
+$(document).on("click", ".city", displayCityInfo);
 //end of document.ready*/
 }); 
 /*
