3072c97df2f75d91ae48038c09d36dadf38a7146
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-01-13 22:28:07 -0500

    added moment.js for more accurate timing in both html and javascript code

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ $(function() {
 
  // Initial array of cities
 var cities = [];
+var currentDate = moment().format('l'); 
 
  function displayCityInfo(){
  var city= $(this).attr("data-name"); 
@@ -16,7 +17,7 @@ var cities = [];
   //Current Day
   let currentWeather = "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png"; 
    $("#icon").attr('src', currentWeather); 
-   $("#selectedCity").text(response.list[0].name + " Time");
+   $("#selectedCity").text(response.list[0].name + " (" + currentDate +")");
    $("#temperature").text("Temperature: " + response.list[0].main.temp + " Fahrenheit"); 
    $("#humidity").text("Humidity: " + response.list[0].main.humidity + " %"); 
    $("#windSpeed").text("Wind Speed: " + response.list[0].wind.speed + " MPH"); 
@@ -29,27 +30,27 @@ $.ajax({
   method: "GET"
 }).then(function(response){
 //day 1 
-$("#day1").text(response.list[0].dt_txt); 
+$("#day1").text(moment(currentDate).add('1', 'day').format('l')); 
 $("#icon1").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png");
 $("#temp1").text("Temperature: "+ response.list[0].main.temp + " Fahrenheit");
 $("#humidity1").text("Humidity: " + response.list[0].main.humidity + " %"); 
 //day 2
-$("#day2").text(response.list[7].dt_txt); 
+$("#day2").text(moment(currentDate).add('2', 'day').format('l')); 
 $("#icon2").attr("src", "http://openweathermap.org/img/wn/" + response.list[7].weather[0].icon + "@2x.png");
 $("#temp2").text("Temperature: "+ response.list[7].main.temp + " Fahrenheit");
 $("#humidity2").text("Humidity: " + response.list[7].main.humidity + " %"); 
 //day 3
-$("#day3").text(response.list[15].dt_txt); 
+$("#day3").text(moment(currentDate).add('3', 'day').format('l')); 
 $("#icon3").attr("src", "http://openweathermap.org/img/wn/" + response.list[15].weather[0].icon + "@2x.png");
 $("#temp3").text("Temperature: "+ response.list[15].main.temp + " Fahrenheit");
 $("#humidity3").text("Humidity: " + response.list[15].main.humidity + " %"); 
 //day 4
-$("#day4").text(response.list[23].dt_txt); 
+$("#day4").text(moment(currentDate).add('4', 'day').format('l')); 
 $("#icon4").attr("src", "http://openweathermap.org/img/wn/" + response.list[23].weather[0].icon + "@2x.png");
 $("#temp4").text("Temperature: "+ response.list[23].main.temp + " Fahrenheit");
 $("#humidity4").text("Humidity: " + response.list[23].main.humidity + " %"); 
 //day 5
-$("#day5").text(response.list[31].dt_txt); 
+$("#day5").text(moment(currentDate).add('5', 'day').format('l')); 
 $("#icon5").attr("src", "http://openweathermap.org/img/wn/" + response.list[31].weather[0].icon + "@2x.png");
 $("#temp5").text("Temperature: "+ response.list[31].main.temp + " Fahrenheit");
 $("#humidity5").text("Humidity: " + response.list[31].main.humidity + " %"); 
