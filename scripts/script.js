10134029a855469139266af2d81bbf5972ac532f
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-01-14 19:51:36 -0500

    fixed my api calls so they are more functional, need to reduce the code if possible and fix my dates somehow

--- scripts/script.js
+++ scripts/script.js
@@ -6,57 +6,66 @@ var currentDate = moment().format('l');
 
  function displayCityInfo(){
  var city= $(this).attr("data-name"); 
- var queryURL = "http://api.openweathermap.org/data/2.5/find?q=" + city + "&units=imperial&appid=101220419d85ffb610459f1145df78ff";
- var queryURL5Day = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=101220419d85ffb610459f1145df78ff";
- 
+ var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=101220419d85ffb610459f1145df78ff" 
 
  $.ajax({
- url: queryURL,
- method: "GET"
- }).then(function(response) {
-  //Current Day
-  let currentWeather = "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png"; 
-   $("#icon").attr('src', currentWeather); 
-   $("#selectedCity").text(response.list[0].name + " (" + currentDate +")");
-   $("#temperature").text("Temperature: " + response.list[0].main.temp + " Fahrenheit"); 
-   $("#humidity").text("Humidity: " + response.list[0].main.humidity + " %"); 
-   $("#windSpeed").text("Wind Speed: " + response.list[0].wind.speed + " MPH"); 
-  // $("#uvIndex").text("UV Index: " + response.list)
+   url: geoCode,
+   method: "GET"
+ }).then(function(response){
+   console.log(response); 
+   var lat = response[0].lat
+   var lon = response[0].lon
+   var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=101220419d85ffb610459f1145df78ff"
+
+   $.ajax({
+     url: queryURL, 
+     methond: "GET"
+   }).then(function(response){
+    //current
+    let currentWeather = "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png"; 
+    $("#icon").attr('src', currentWeather); 
+    $("#selectedCity").text(city + " (" + currentDate +")");
+    $("#temperature").text("Temperature: " + response.current.temp + " Fahrenheit"); 
+    $("#humidity").text("Humidity: " + response.current.humidity + " %"); 
+    $("#windSpeed").text("Wind Speed: " + response.current.wind_speed + " MPH"); 
+    $("#uvIndex").text("UV Index: " + response.current.uvi); 
+    //5Day
+    //day 1 
+    $("#day1").text(moment(currentDate).add('1', 'day').format('l')); 
+    $("#icon1").attr("src", "http://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon + "@2x.png");
+    $("#temp1").text("Temperature: "+ response.daily[0].temp.day + " Fahrenheit");
+    $("#humidity1").text("Humidity: " + response.daily[0].humidity + " %"); 
+
+    //day 2
+    $("#day2").text(moment(currentDate).add('2', 'day').format('l')); 
+    $("#icon2").attr("src", "http://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + "@2x.png");
+    $("#temp2").text("Temperature: "+ response.daily[1].temp.day + " Fahrenheit");
+    $("#humidity2").text("Humidity: " + response.daily[1].humidity + " %"); 
+
+    //day 3
+    $("#day3").text(moment(currentDate).add('3', 'day').format('l')); 
+    $("#icon3").attr("src", "http://openweathermap.org/img/wn/" + response.daily[2].weather[0].icon + "@2x.png");
+    $("#temp3").text("Temperature: "+ response.daily[2].temp.day + " Fahrenheit");
+    $("#humidity3").text("Humidity: " + response.daily[2].humidity + " %"); 
+
+    //day4
+    $("#day4").text(moment(currentDate).add('4', 'day').format('l')); 
+    $("#icon4").attr("src", "http://openweathermap.org/img/wn/" + response.daily[3].weather[0].icon + "@2x.png"); 
+    $("#temp4").text("Temperature: "+ response.daily[3].temp.day + " Fahrenheit");
+    $("#humidity4").text("Humidity: " + response.daily[3].humidity + " %"); 
+
+    //day 5
+    $("#day5").text(moment(currentDate).add('5', 'day').format('l')); 
+    $("#icon5").attr("src", "http://openweathermap.org/img/wn/" + response.daily[4].weather[0].icon + "@2x.png");
+    $("#temp5").text("Temperature: "+ response.daily[4].temp.day + " Fahrenheit");
+    $("#humidity5").text("Humidity: " + response.daily[4].humidity + " %"); 
   
-});
-//5 day
-$.ajax({
-  url: queryURL5Day, 
-  method: "GET"
-}).then(function(response){
-//day 1 
-$("#day1").text(moment(currentDate).add('1', 'day').format('l')); 
-$("#icon1").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png");
-$("#temp1").text("Temperature: "+ response.list[0].main.temp + " Fahrenheit");
-$("#humidity1").text("Humidity: " + response.list[0].main.humidity + " %"); 
-//day 2
-$("#day2").text(moment(currentDate).add('2', 'day').format('l')); 
-$("#icon2").attr("src", "http://openweathermap.org/img/wn/" + response.list[7].weather[0].icon + "@2x.png");
-$("#temp2").text("Temperature: "+ response.list[7].main.temp + " Fahrenheit");
-$("#humidity2").text("Humidity: " + response.list[7].main.humidity + " %"); 
-//day 3
-$("#day3").text(moment(currentDate).add('3', 'day').format('l')); 
-$("#icon3").attr("src", "http://openweathermap.org/img/wn/" + response.list[15].weather[0].icon + "@2x.png");
-$("#temp3").text("Temperature: "+ response.list[15].main.temp + " Fahrenheit");
-$("#humidity3").text("Humidity: " + response.list[15].main.humidity + " %"); 
-//day 4
-$("#day4").text(moment(currentDate).add('4', 'day').format('l')); 
-$("#icon4").attr("src", "http://openweathermap.org/img/wn/" + response.list[23].weather[0].icon + "@2x.png");
-$("#temp4").text("Temperature: "+ response.list[23].main.temp + " Fahrenheit");
-$("#humidity4").text("Humidity: " + response.list[23].main.humidity + " %"); 
-//day 5
-$("#day5").text(moment(currentDate).add('5', 'day').format('l')); 
-$("#icon5").attr("src", "http://openweathermap.org/img/wn/" + response.list[31].weather[0].icon + "@2x.png");
-$("#temp5").text("Temperature: "+ response.list[31].main.temp + " Fahrenheit");
-$("#humidity5").text("Humidity: " + response.list[31].main.humidity + " %"); 
-});
- } 
-  
+  });
+
+   });
+  };
+
+ 
 
 
 //Looping through the array of cities 
@@ -83,8 +92,9 @@ $("#searchBtn").on("click", function(event) {
     renderButtons(); 
     console.log(cityHistory); 
    });
- 
 
+//why isnt working? 
+$(document).on("click", "#searchBtn", displayCityInfo); 
 $(document).on("click", ".city", displayCityInfo);
 //end of document.ready*/
 }); 
