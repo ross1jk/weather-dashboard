a1f91fa85fa88de8d92195f8b24c06f36045ebf2
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-01-15 20:54:07 -0500

    updated link to for ajax call to see if that helps with rending the page live

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ var currentDate = moment().format('l');
   
  var city= $(this).attr("data-name") || "Flint"; 
 
- var geoCode = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=101220419d85ffb610459f1145df78ff" 
+ var geoCode = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=101220419d85ffb610459f1145df78ff" 
 
  $.ajax({
    url: geoCode,
@@ -24,7 +24,7 @@ var currentDate = moment().format('l');
      methond: "GET"
    }).then(function(response){
     //current
-    let currentWeather = "http://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png"; 
+    let currentWeather = "https://openweathermap.org/img/wn/" + response.current.weather[0].icon + "@2x.png"; 
     $("#icon").attr('src', currentWeather); 
     $("#selectedCity").text(city + " (" + currentDate +")");
     $("#temperature").text("Temperature: " + response.current.temp + " Fahrenheit"); 
@@ -33,36 +33,36 @@ var currentDate = moment().format('l');
     //5Day
     //day 1 
     $("#day1").text(moment(currentDate).add('1', 'day').format('l')); 
-    $("#icon1").attr("src", "http://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon + "@2x.png");
+    $("#icon1").attr("src", "https://openweathermap.org/img/wn/" + response.daily[0].weather[0].icon + "@2x.png");
     $("#temp1").text("Temperature: "+ response.daily[0].temp.day + " Fahrenheit");
     $("#humidity1").text("Humidity: " + response.daily[0].humidity + " %"); 
 
     //day 2
     $("#day2").text(moment(currentDate).add('2', 'day').format('l')); 
-    $("#icon2").attr("src", "http://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + "@2x.png");
+    $("#icon2").attr("src", "https://openweathermap.org/img/wn/" + response.daily[1].weather[0].icon + "@2x.png");
     $("#temp2").text("Temperature: "+ response.daily[1].temp.day + " Fahrenheit");
     $("#humidity2").text("Humidity: " + response.daily[1].humidity + " %"); 
 
     //day 3
     $("#day3").text(moment(currentDate).add('3', 'day').format('l')); 
-    $("#icon3").attr("src", "http://openweathermap.org/img/wn/" + response.daily[2].weather[0].icon + "@2x.png");
+    $("#icon3").attr("src", "https://openweathermap.org/img/wn/" + response.daily[2].weather[0].icon + "@2x.png");
     $("#temp3").text("Temperature: "+ response.daily[2].temp.day + " Fahrenheit");
     $("#humidity3").text("Humidity: " + response.daily[2].humidity + " %"); 
 
     //day4
     $("#day4").text(moment(currentDate).add('4', 'day').format('l')); 
-    $("#icon4").attr("src", "http://openweathermap.org/img/wn/" + response.daily[3].weather[0].icon + "@2x.png"); 
+    $("#icon4").attr("src", "https://openweathermap.org/img/wn/" + response.daily[3].weather[0].icon + "@2x.png"); 
     $("#temp4").text("Temperature: "+ response.daily[3].temp.day + " Fahrenheit");
     $("#humidity4").text("Humidity: " + response.daily[3].humidity + " %"); 
 
     //day 5
     $("#day5").text(moment(currentDate).add('5', 'day').format('l')); 
-    $("#icon5").attr("src", "http://openweathermap.org/img/wn/" + response.daily[4].weather[0].icon + "@2x.png");
+    $("#icon5").attr("src", "https://openweathermap.org/img/wn/" + response.daily[4].weather[0].icon + "@2x.png");
     $("#temp5").text("Temperature: "+ response.daily[4].temp.day + " Fahrenheit");
     $("#humidity5").text("Humidity: " + response.daily[4].humidity + " %"); 
   
   });
-   let uvi = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon +"&appid=101220419d85ffb610459f1145df78ff"
+   let uvi = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon +"&appid=101220419d85ffb610459f1145df78ff"
    $.ajax({
      url: uvi, 
      method: "GET"
